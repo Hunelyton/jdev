f8c433b10eabf2af178e8ab166cef9d6f63b84e9
Hunelyton <hunelyton@hotmail.com>
2024-02-13 16:10:26 -0300

    Adicionando primeiros arquivos

---
+++ modulo7/src/1tiposevariaveis.java
@@ -0,0 +1,15 @@
+//tipo e variáveis
+
+//Tipo: Entendemos por tipos de informação
+//variáveis: uma variável refenciará o tipo de dados
+
+//no exemplo abaixo vamos ver quando a palavra "byte" que se enquadra no tipo e "tipoByte" como a declaração de variável
+
+
+
+byte tipoByte;
+short tipoShort;
+int tipoInt;
+boolean tipoBoolean;
+String tipoTexto;
+BigDecimal tipoBigDecimal;
